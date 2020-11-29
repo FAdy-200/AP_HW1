e22789eab05da5ca39763dee3218a7f633694d1d
FAdy200 <abuyehia200@gmail.com>
2020-11-29 15:38:16 +0200

    WORKING BUT NEED DOCUMENTATIONS AND SOME REFINEMENTS

--- Connect4/Connect4.py
+++ Connect4/Connect4.py
@@ -3,67 +3,60 @@ import re
 
 class Connect4:
     def __init__(self):
-        self.grid = list(("O" * 7 + ".") * 6)
-        self.player = "R"
+        self.gridMutable = list(("O" * 7 + ".") * 6)
+        self.grid = "".join(self.gridMutable)
+        self.playerTurn = "R"
         self.gameOn = True
         self.rounds = 0
 
-    def _Changeplayer(self):
-        if self.player == "R":
-            self.player = "Y"
+    def _ChangePlayer(self):
+        if self.playerTurn == "R":
+            self.playerTurn = "Y"
         else:
-            self.player = "R"
+            self.playerTurn = "R"
 
-    def make_move(self, move):
+    def _return_move_place(self, move):
         if move > 7:
             print("Move out of range")
-            return
-        while True:
-            try:
-                if self.grid[move] != "O":
-                    move += 8
-                else:
-                    self.grid[move] = self.player
-                    self.rounds += 1
-                    self._checkWinStatus()
-                    self._Changeplayer()
-                    break
-            except:
-                print("Move out of range")
-                break
+            return None
+        movePlace = move
+        try:
+            while self.grid[movePlace] != "O":
+                movePlace += 8
+            return movePlace
+        except IndexError:
+            print("Move out of range")
+            return None
+    def make_move(self, move):
+        movePlace = self._return_move_place(move)
+        if movePlace != None:
+            self.gridMutable[movePlace] = self.playerTurn
+            self.grid = "".join(self.gridMutable)
+            self.rounds += 1
+            self._checkWinStatus()
+            self._ChangePlayer()
         if self.rounds == 42:
             self.gameOn = False
             print("Draw no one won")
 
     def _checkWinStatus(self):
-        p1 = self.player * 4
-        p3 = self.player + ".{8}" + self.player + ".{8}" + self.player + ".{8}" + self.player
-        c1 = re.search(p1, "".join(self.grid))
-        c3 = re.search(p3, "".join(self.grid))
-        if c1 or c3:
-            print(self.player, "Has won the game")
+        p1 = self.playerTurn * 4
+        p2 = self.playerTurn + ".{7}" + self.playerTurn + ".{7}" + self.playerTurn + ".{7}" + self.playerTurn
+        p3 = self.playerTurn + ".{6}" + self.playerTurn + ".{6}" + self.playerTurn + ".{6}" + self.playerTurn
+        p4 = self.playerTurn + ".{8}" + self.playerTurn + ".{8}" + self.playerTurn + ".{8}" + self.playerTurn
+        c1 = re.search(p1, self.grid)
+        c2 = re.search(p2, self.grid)
+        c3 = re.search(p3, self.grid)
+        c4 = re.search(p4, self.grid)
+        if c1 or c2 or c3 or c4:
+            print(self.playerTurn, "Has won the game")
             self.gameOn = False
 
     def printBoard(self):
-        j = 1
-        tempGrid = []
-        g = []
-        i = 0
-        while i < len(self.grid):
-            if j % 7 == 0:
-                g.append(self.grid[i])
-                tempGrid.append("".join(g))
-                g = []
-                j += 1
-                i += 2
-
-            else:
-
-                g.append(self.grid[i])
-                j += 1
-                i += 1
-        for i in range(1, len(tempGrid) + 1):
-            print(tempGrid[-i])
+        gridUpside = self.grid[::-1]
+        gridRightSide = gridUpside.split(".")
+        for i in gridRightSide:
+            print(i[::-1])
 
 
 s = Connect4()
@@ -72,8 +65,8 @@ import random
 
 for _ in range(int(input())):
     while s.gameOn:
-        # m = int(input("\nIt is {} turn please input the place where you want to drop your circle\n".format(s.player)))
+        # m = int(input("\nIt is {} turn please input the place where you want to drop your circle\n".format(s.playerTurn)))
         m = random.randint(0, 6)
         s.make_move(m)
-    # s.printBoard()
+    s.printBoard()
     s = Connect4()
