6a3b8b3856d53c0626a40c50bd7c6b030f485b79
FAdy200 <abuyehia200@gmail.com>
2020-12-04 20:10:32 +0200

    DONE

--- Rock_Paper_Scissors/Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors/Rock_Paper_Scissors.py
@@ -7,59 +7,61 @@ class Rock_Paper_Scissors:
         self.aiWins = 0
         self.humanWins = 0
         self.roundsPlayed = 0
+        self.roundsLeft = self.rounds - self.roundsPlayed
         self.humanChoice = None
         self.aiChoice = None
         self.order = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
         self.gameOn = True
+        self.lastRoundWinner = ""
 
-    def getRoundWinner(self):
+    def get_round_winner(self):
         if self.order[self.humanChoice] == self.aiChoice:
             self.humanWins += 1
-            self.roundsPlayed += 1
-            return "Human"
+
+            self.lastRoundWinner = "Human"
         elif self.order[self.aiChoice] == self.humanChoice:
             self.aiWins += 1
-            self.roundsPlayed += 1
-            return "Computer"
+            self.lastRoundWinner = "Computer"
         else:
-            self.roundsPlayed += 1
-            return "Draw"
+            self.lastRoundWinner = "Draw"
+        self.roundsPlayed += 1
+        self.roundsLeft -= 1
 
-    def getGameWinner(self):
-        roundsLeft = self.rounds - self.roundsPlayed
-        if abs(self.humanWins - self.aiWins) >= roundsLeft + 1:
+    def get_game_winner(self):
+        if abs(self.humanWins - self.aiWins) >= self.roundsLeft + 1:
             if self.humanWins > self.aiWins:
                 print("The human has won the game")
                 self.gameOn = False
                 return "Human"
-            else:
+            elif self.humanWins < self.aiWins:
                 print("The computer has won the game")
                 self.gameOn = False
                 return "Computer"
-        else:
-            return
+        elif self.roundsLeft == 0:
+            print("The game was a draw no one won")
+            self.gameOn = False
+            return "Draw"
 
-    def __getAiChoice(self):
+    def __get_computer_choice(self):
         d = {0: "Scissors", 1: "Rock", 2: "Paper"}
         x = random.randint(0, 2)
         self.aiChoice = d[x]
 
-    def playAround(self, choice):
+    def play_a_round(self, choice):
         self.humanChoice = choice
-        self.__getAiChoice()
-        w = self.getRoundWinner()
+        self.__get_computer_choice()
+        w = self.get_round_winner()
         if w != "Draw":
             print("Computer choose {}".format(self.aiChoice))
             print("{} has won this round".format(w))
         else:
             print("it was a draw the round will be counted form the total game played")
-        self.getGameWinner()
-
+        self.get_game_winner()
 
 # d = {0: "Scissors", 1: "Rock", 2: "Paper"}
-# # n = int(input())
-# n = 100000
+# n = int(input())
+# # n = 100000
 # r = Rock_Paper_Scissors(n)
 # while r.gameOn:
-#     r.playAround(d[random.randint(0, 2)])
-#     # r.playAround(d[int(input())])
+# #     r.playAround(d[random.randint(0, 2)])
+#     r.playAround(d[int(input())])
